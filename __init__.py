fad446ae6e1d2c80c9c1c7cbe908cdf14e3f15c2
akerue <hsmgn8118akerue@gmail.com>
2016-02-10 02:40:15 +0900

    スクレーピング処理実装

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#_*_coding:utf-8_*_
+
+
+
+if __name__ == "__main__":
+
---
+++ main.py
@@ -0,0 +1,90 @@
+# _*_coding:utf-8_*_
+
+import pprint
+import sys
+import csv
+import logging
+from pyquery import PyQuery as pq
+
+
+def crawler(home_url):
+    """
+    「next >>」リンクがあればそこに飛び、scraperを呼ぶ
+    なければ終了
+    """
+    shop_info_list = []
+    page_num = 1
+
+    while True:
+        url = home_url+"page:{}".format(page_num)
+        logging.info("次のページをスクレーピングします")
+        logging.info(url)
+        shop_info_list.extend(scraper(url))
+        dom = pq(url=url)
+        if dom.find(".next"):
+            logging.info("次のページへ遷移します")
+            page_num += 1
+            continue
+        else:
+            logging.info("終了します")
+            break
+
+    return shop_info_list
+
+
+def scraper(page_url):
+    """
+    ショップ一覧から情報を取得する
+    取得情報
+    {"name": ショップ名,
+     "address": 住所,
+     "link": リンク先}
+    """
+    shop_list = []
+    dom = pq(page_url)
+
+    # ショップの名前とリンク一覧取得
+    names = []
+    links = []
+    for shop_name in dom("li.shop_name a"):
+        query = pq(shop_name)
+        names.append(query.text())
+        links.append(query.attr("href"))
+
+    # 住所一覧取得
+    addresses = []
+    for address in dom(".shop_info").items():
+        addresses.append(address.text().lstrip(u"住所:"))
+
+    # ループでくっつける
+    for name, link, address in zip(names, links, addresses):
+        logging.debug("-----------------------")
+        logging.debug("NAME: {}".format(name.encode("utf-8")))
+        logging.debug("ADDRESS: {}".format(address.encode("utf-8")))
+        logging.debug("LINK: {}".format(link.encode("utf-8")))
+        logging.debug("-----------------------")
+        shop_list.append({"name": name,
+                          "link": link,
+                          "address": address,
+                          })
+    return shop_list
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+
+    params = sys.argv
+
+    # 簡単に引数チェック
+    try:
+        pref_num = params[1]
+        assert(0 < int(pref_num) and int(pref_num) < 48)
+    except Exception, e:
+        print "---------USAGE--------"
+        print "python main.py [pref_num]"
+        print "pref_num: from 1 to 47"
+        print "----------------------"
+        raise(e)
+
+    url = "http://www.fashion-press.net/shops/pref_{pref_num}/".format(
+                                                            pref_num=pref_num)
+    pprint.pprint(crawler(url))
